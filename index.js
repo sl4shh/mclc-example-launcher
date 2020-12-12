289e9488ff796cd8b73daded2db9f43e85bb6b97
SlashDev <43612518+SlashDevv@users.noreply.github.com>
2020-12-12 14:48:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
+// Importation des modules
 const { app, ipcMain, BrowserWindow } = require("electron");
 const path = require("path");
+const { Client, Authenticator } = require("minecraft-launcher-core");
 
+// Variables globales
 let mainWindow;
 
+// Création de la fenêtre principale
 function createWindow() {
   mainWindow = new BrowserWindow({
-    //frame: false,
     title: "Lanceur MC",
-    icon: path.join(__dirname, "/asset/logo.ico"),
+    icon: path.join(__dirname, "/asset/logo.png"),
     width: 800,
     height: 600,
+    frame: false,
     webPreferences: {
       nodeIntegration: true,
+      enableRemoteModule: true,
     },
   });
 
   mainWindow.loadURL(path.join(__dirname, "index.html"));
 }
 
+// Quand l'application est chargée, afficher la fenêtre
 app.whenReady().then(() => {
   createWindow();
 
@@ -26,6 +32,7 @@ app.whenReady().then(() => {
   });
 });
 
+// Si toutes les fenêtres sont fermées, quitter l'application
 app.on("window-all-closed", function () {
   if (process.platform !== "darwin") app.quit();
 });
